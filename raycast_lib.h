736da3ab32ce0e2df84e1ae85659de8462e016ff
Moon <mp2893@nau.edu>
2026-02-17 18:00:53 -0700

    Updated raycast

--- raycast_lib.h
+++ raycast_lib.h
@@ -10,26 +10,40 @@
 #include <fstream>
 #include <sstream>
 
-typedef struct sceneData{
-    std::string magicNum;
+#define MAX_OBJECTS 128
+#define EPSILON 1e-6f
 
-    float c_diff[3];      // diffuse color
-    float x, y, z;        // position for sphere/plane
 
-    float cam_width;      // camera width:
-    float cam_height;     // camera height:
+enum ObjectType {
+    OBJ_CAMERA,
+    OBJ_SPHERE,
+    OBJ_PLANE
+};
+
+
+typedef struct sceneData {
+    ObjectType type;
+
+    float c_diff[3];     
+    float position[3];   
+
+    // camera properties
+    float cam_width;
+    float cam_height;
+
 } sceneData;
 
-typedef struct sphere : sceneData{
+
+typedef struct sphere : sceneData {
     float radius;
 } sphere;
 
-typedef struct plane : sceneData{
-    float normal[3];      // MUST be 3D
+
+typedef struct plane : sceneData {
+    float normal[3];   // 3D normal vector
 } plane;
 
 
-// vector library (as you already had)
 void  v3_from_points(float *dst, float *a, float *b);
 void  v3_add(float *dst, float *a, float *b);
 void  v3_subtract(float *dst, float *a, float *b);
@@ -41,7 +55,6 @@ float v3_angle_quick(float *a, float *b);
 void  v3_reflect(float *dst, float *v, float *n);
 float v3_length(float *a);
 void  v3_normalize(float *dst, float *a);
-
 bool  v3_equals(float *a, float *b, float tolerance);
 
 #endif // RAYCAST_LIB_H
