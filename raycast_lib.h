e616d568e70eeba8e93e6e3dd21aec90f42072df
liamO2049 <lko27@nau.edu>
2026-02-17 17:46:56 -0700

    wfe

--- raycast_lib.h
+++ raycast_lib.h
@@ -8,44 +8,39 @@
 #include <math.h>
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <sstream>
 
-class SceneObject {
-public:
-    virtual ~SceneObject() = default;
+typedef struct sceneData{
+    std::string magicNum;
 
-    virtual void render() = 0;   // or intersect(), update(), etc.
-};
+    float c_diff[3];      // diffuse color
+    float x, y, z;        // position for sphere/plane
 
-class sphere : SceneObject{
+    float cam_width;      // camera width:
+    float cam_height;     // camera height:
+} sceneData;
+
+typedef struct sphere : sceneData{
     float radius;
+} sphere;
+
+typedef struct plane : sceneData{
+    float normal[3];      // MUST be 3D
+} plane;
 
 
-<<<<<<< Updated upstream
-}
-
-class plane : SceneObject{
-    float normal;
-
-}
-
-
-void  v3_from_points(float *dst, float *a, float *b);  
-=======
+// vector library (as you already had)
 void  v3_from_points(float *dst, float *a, float *b);
->>>>>>> Stashed changes
 void  v3_add(float *dst, float *a, float *b);
 void  v3_subtract(float *dst, float *a, float *b);
 float v3_dot_product(float *a, float *b);
 void  v3_cross_product(float *dst, float *a, float *b);
 void  v3_scale(float *dst, float s);
-float v3_angle(float *a, float *b);                      
-float v3_angle_quick(float *a, float *b);               
-void  v3_reflect(float *dst, float *v, float *n);         
+float v3_angle(float *a, float *b);
+float v3_angle_quick(float *a, float *b);
+void  v3_reflect(float *dst, float *v, float *n);
 float v3_length(float *a);
 void  v3_normalize(float *dst, float *a);
-
 
 bool  v3_equals(float *a, float *b, float tolerance);
 
